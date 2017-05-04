2617247a172d7fbe942beafede4053bedaeddb54
Akira Misawa <ccccccccandy@gmail.com>
2017-05-04 18:17:38 +0900

    added 1-10

---
+++ chap1/1-10.rkt
@@ -0,0 +1,40 @@
+#lang racket
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1) (A x (- y 1))))))
+
+(= 1024 (A 1 10))
+(= 65536 (A 2 4))
+(= 65536 (A 3 3))
+
+;; x is bounded to 0
+;; (f n) : 2 * n
+(define (f n) (A 0 n))
+(= 2 (f 1))
+(= 4 (f 2))
+(= -6 (f -3))
+(= 10 (f 5))
+
+;; x is bounded to 1
+;; (g n) : 2^n
+(define (g n) (A 1 n))
+(= 2 (g 1))
+(= 4 (g 2))
+(= 256 (g 8))
+(= 1024 (g 10))
+
+;; x is bounded to 2
+(define (h n) (A 2 n))
+(= 2 (h 1))
+(= 4 (h 2))
+(= 16 (h 3))
+(= 65536 (h 4))
+
+;; (k n) : 5 * n^2
+(define (k n) (* 5 n n))
+(= 5 (k 1))
+(= 5 (k -1))
+(= 20 (k 2))
+(= 125 (k 5))
--- chap1/1-3.py
+++ chap1/1-3.py
@@ -10,9 +10,14 @@ import unittest
 '''
 
 
-def sum_of_squares_of_two_larger_numbers_from_three(a, b, c):
+def take_two_large_numbers(a, b, c):
     lst = [a, b, c]
-    two_largers = sorted(lst, reverse=True)
+    ret = sorted(lst, reverse=True)
+    return ret
+
+
+def sum_of_squares_of_two_larger_numbers_from_three(a, b, c):
+    two_largers = take_two_large_numbers(a, b, c)
     ret = two_largers[0] ** 2 + two_largers[1] ** 2
     return ret
 
@@ -20,15 +25,24 @@ def sum_of_squares_of_two_larger_numbers_from_three(a, b, c):
 class UnitTestOfAboveFunction(unittest.TestCase):
     def test_case_1(self):
         self.assertEqual(
-            13, sum_of_squares_of_two_larger_numbers_from_three(3, 2, 1))
+            13,
+            sum_of_squares_of_two_larger_numbers_from_three(3, 2, 1))
+        self.assertEqual(
+            13,
+            sum_of_squares_of_two_larger_numbers_from_three(1, 2, 3))
+        self.assertEqual(
+            13,
+            sum_of_squares_of_two_larger_numbers_from_three(2, 1, 3))
 
     def test_case_2(self):
         self.assertEqual(
-            97, sum_of_squares_of_two_larger_numbers_from_three(-2, 4, 9))
+            97,
+            sum_of_squares_of_two_larger_numbers_from_three(-2, 4, 9))
 
     def test_case_3(self):
         self.assertEqual(
-            90, sum_of_squares_of_two_larger_numbers_from_three(-10, -9, 3))
+            90,
+            sum_of_squares_of_two_larger_numbers_from_three(-10, -9, 3))
 
 
 if __name__ == '__main__':
