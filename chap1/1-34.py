9e102bc231ca084a33b5fbca17b4794938b1aac2
matsueushi <sinxcosxtanx@gmail.com>
2017-05-30 23:37:33 +0900

    Add 1-34,1-35

---
+++ chap1/1-34.py
@@ -0,0 +1,12 @@
+def f(g):
+    return g(2)
+
+print(f(lambda x:x*x))
+print(f(lambda x:x*(x+1)))
+#print(f(f)) TypeError: 'int' object is not callable
+# f(g)の宣言でgに2を適用していることから、gの定義域はNumeric Type
+# g:Numeric->Tの形になる。従ってfは
+# f:Functor(Numeric->T)->T
+#　の形になる
+# f自体はFunctor(Numeric->T)の形ではないので、
+# f(f)は呼び出せない
