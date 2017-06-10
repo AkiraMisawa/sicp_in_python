9a95b336f1604526a12fba6dc47f29868a1b5c17
panishi <knpc1101@yahoo.co.jp>
2017-06-11 00:16:54 +0900

    Add utility.py.

---
+++ chap1/utility.py
@@ -0,0 +1,68 @@
+# coding=utf-8
+"""
+For utility functions.
+"""
+
+
+def identity(x):
+    """
+    :type x: float
+    :rtype: float
+    """
+    return x
+
+
+def square(x):
+    """
+    :type x: float
+    :rtype: float
+    """
+    return x * x
+
+
+def cube(x):
+    """
+    :type x: float
+    :rtype: float
+    """
+    return x * x * x
+
+
+def inc(x):
+    """
+    :type x: float
+    :rtype: float
+    """
+    return x + 1
+
+
+def dec(x):
+    """
+    :type x: float
+    :rtype: float
+    """
+    return x - 1
+
+
+def double(x):
+    """
+    :type x: float
+    :rtype: float
+    """
+    return 2 * x
+
+
+def halve(x):
+    """
+    :type x: int
+    :rtype: int
+    """
+    return x // 2
+
+
+def is_even(x):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    return x % 2 == 0
