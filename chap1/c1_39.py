85a2da5eebe464ed574bce159a6185e4b4839bd0
matsueushi <sinxcosxtanx@gmail.com>
2017-06-12 22:02:40 +0900

    Modify 1-35, 39

--- chap1/c1_39.py
+++ chap1/c1_39.py
@@ -1,13 +1,23 @@
 import c1_37 as func
 
-def tan_cf(x,k):
-    ni = lambda i: -x ** 2
-    di = lambda i: 2 * i + 1
-    return x/(1+func.cont_frac_recur(ni,di,k))
+
+def tan_cf(x, k):
+    def ni(i):
+        if i == 1:
+            return x
+        else:
+            return -x ** 2
+
+    def di(i):
+        return 2 * i - 1
+
+    return x/(1+func.cont_frac_recur(ni, di, k))
+
 
 def main():
-    for r in range(1,20):
-        print(r,tan_cf(0.5,r))
+    for r in range(1, 20):
+        print(r, tan_cf(0.5, r))
+
 
 if __name__ == '__main__':
     main()
