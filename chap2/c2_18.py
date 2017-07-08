0083362a606259153122937590b8ecfedb014325
matsueushi <sinxcosxtanx@gmail.com>
2017-07-08 10:34:31 +0900

    Modify variable names

--- chap2/c2_18.py
+++ chap2/c2_18.py
@@ -2,29 +2,29 @@ import utility
 import unittest
 
 
-def reverse(l):
-    if utility.is_null(l):
+def reverse(lst):
+    if utility.is_null(lst):
         return None
-    elif utility.length(l) == 1:
-        return l
+    elif utility.length(lst) == 1:
+        return lst
     else:
-        x = utility.car(l)
-        xs = utility.cdr(l)
+        x = utility.car(lst)
+        xs = utility.cdr(lst)
         return utility.append(reverse(xs), utility.list(x))
 
 
 class UnitTestOfAboveFunctions(unittest.TestCase):
     def test_case1(self):
-        l = utility.list(1, 2, 3, 4)
-        l_rev = reverse(l)
+        lst = utility.list(1, 2, 3, 4)
+        l_rev = reverse(lst)
         l_rev_ans = utility.list(4, 3, 2, 1)
         self.assertTrue(utility.is_same_list(l_rev, l_rev_ans))
 
     def test_case2(self):
         a = utility.list(1, 2)
         b = utility.list(3, 4)
-        l = utility.list(a, b)
-        l_rev = reverse(l)
+        lst = utility.list(a, b)
+        l_rev = reverse(lst)
         l_rev_ans = utility.list(b, a)
         self.assertTrue(utility.is_same_list(l_rev, l_rev_ans))
 
--- chap2/c2_27.py
+++ chap2/c2_27.py
@@ -2,24 +2,24 @@ import utility
 import unittest
 
 
-def deep_reverse(l):
+def deep_reverse(lst):
     def deep_reverse_element(a):
         if callable(a):
             return utility.list(deep_reverse(a))
         else:
             return utility.list(a)
 
-    def deep_reverse_of_length_one(l):
-        return deep_reverse_element(utility.car(l))
+    def deep_reverse_of_length_one(lst):
+        return deep_reverse_element(utility.car(lst))
 
-    utility.print_list(l)
-    if utility.is_null(l):
+    utility.print_list(lst)
+    if utility.is_null(lst):
         return None
-    elif utility.length(l) == 1:
-        return deep_reverse_of_length_one(l)
+    elif utility.length(lst) == 1:
+        return deep_reverse_of_length_one(lst)
     else:
-        x = utility.car(l)
-        xs = utility.cdr(l)
+        x = utility.car(lst)
+        xs = utility.cdr(lst)
         x_rev = deep_reverse_element(x)
         xs_rev = deep_reverse(xs)
         return utility.append(xs_rev, x_rev)
@@ -27,27 +27,27 @@ def deep_reverse(l):
 
 class UnitTestOfAboveFunctions(unittest.TestCase):
     def test_case1(self):
-        l = utility.list(1, 2, 3, 4)
-        l_rev = deep_reverse(l)
+        lst = utility.list(1, 2, 3, 4)
+        l_rev = deep_reverse(lst)
         l_rev_ans = utility.list(4, 3, 2, 1)
         self.assertTrue(utility.is_same_list(l_rev, l_rev_ans))
 
     def test_case2(self):
         a = utility.list(1, 2)
         b = utility.list(3, 4)
-        l = utility.list(a, b)
+        lst = utility.list(a, b)
         ll = utility.list(utility.list(1, 2), utility.list(3, 4))
-        self.assertTrue(utility.is_same_list(l, ll))
+        self.assertTrue(utility.is_same_list(lst, ll))
 
     def test_case3(self):
         a = utility.list(1, 2)
         b = utility.list(3, 4)
-        l = utility.list(a, b)
-        l_rev = deep_reverse(l)
+        lst = utility.list(a, b)
+        l_rev = deep_reverse(lst)
         l_rev_ans = utility.list(utility.list(4, 3), utility.list(2, 1))
         utility.print_list(l_rev)
         ll = utility.list(utility.list(1, 2), utility.list(3, 4))
-        self.assertTrue(utility.is_same_list(l, ll))
+        self.assertTrue(utility.is_same_list(lst, ll))
         self.assertTrue(utility.is_same_list(l_rev, l_rev_ans))
 
 
