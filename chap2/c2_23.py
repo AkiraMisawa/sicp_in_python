e98b7a7aa1b9e4b18a416ec2fc8e53c5744b5813
matsueushi <sinxcosxtanx@gmail.com>
2017-07-08 10:37:27 +0900

    Modify variable names

--- chap2/c2_23.py
+++ chap2/c2_23.py
@@ -1,15 +1,14 @@
 import utility
 
 
-def for_each(f, l):
-    if not utility.is_null(l):
-        f(utility.car(l))
-        for_each(f, utility.cdr(l))
+def for_each(f, lst):
+    if not utility.is_null(lst):
+        f(utility.car(lst))
+        for_each(f, utility.cdr(lst))
 
 
 def main():
-    def f(x): return print(x)
-    for_each(f, utility.list(57, 321, 88))
+    for_each(print, utility.list(57, 321, 88))
 
 
 if __name__ == '__main__':
